5e15585ef6481f13d97e77b49db0ffdbc6f665c3
mahmoud emad <81238438+Omda01@users.noreply.github.com>
2024-06-21 23:55:17 +0300

    Update WDGM.C

--- WDGM.C
+++ WDGM.C
@@ -8,11 +8,14 @@
 
 */
 #include "Std_Types.h"
+#include "WDGM.h"
 
+static WDGM_StatusType WDGM_Status = NOK;
+volatile unsigned char LEDM_CallCount = 0;
 
 void WDGM_Init(void)
 {
-
+ LEDM_CallCount = 0;
 }
 
 void WDGM_MainFunction(void)
@@ -22,7 +25,7 @@ void WDGM_MainFunction(void)
 
 void  WDGM_AlivenessIndication(void)
 {
-
+ LEDM_CallCount++;
 }
 
 

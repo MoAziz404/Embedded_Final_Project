d80ac4f3472e5d7ec3ac09639eb607d4f3692881
mahmoud emad <81238438+Omda01@users.noreply.github.com>
2024-06-21 23:45:21 +0300

    Update WDGDRV.C

--- WDGDRV.C
+++ WDGDRV.C
@@ -4,17 +4,26 @@
     MOHAMED AZIZ                SEC: 2      BN: 18
     OMAR MOHAMED MAHMOUD        SEC: 1      BN: 47
 
-    TEAM NUMBER: 
+    TEAM NUMBER: MAHMOUD EMAD
 
 */
 #include "Std_Types.h"
+#include <avr/io.h>
+#include <avr/wdt.h>
+#include "WDGDRV.h"
+#include "WDGM.h"
 
 
 void WDGDrv_Init(void)
 {
+WDTCSR |= (1 << WDCE) | (1 << WDE);  // Enable modifications
+WDTCSR = (1 << WDIE) | (1 << WDP1); // 64ms timeout
     
 }
-void WDGDrv_IsrNotification(void)
-{
+void WDGDrv_IsrNotification(void){
+ if (WDGM_PovideSuppervisionStatus() == OK && !WDGM_MainFunction()){
+     
+         wdt_reset(); 
+    }
 
 }

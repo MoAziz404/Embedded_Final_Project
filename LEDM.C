b84fd2ed42eec7a15b91fdefbdf1d95a4ec90a21
Omarmo2025 <166153836+Omarmo2025@users.noreply.github.com>
2024-06-22 01:41:50 +0300

    Update LEDM.C

--- LEDM.C
+++ LEDM.C
@@ -7,14 +7,31 @@
     TEAM NUMBER: 
 
 */
-#include "Std_Types.h"
+ 
+#include <util/delay.h>
+#include "LEDMgr.h"
+#include "GPIO.h"
+#include "WDGM.h"
 
+static unsigned int Counter = 0;
+static unsigned char led_State = 0;
 
-void LEDM_Init(void)
-{
-
+void LED_Init(void) {
+    GPIO_Init();
+    Counter = 0;
+    led_State = 0;
 }
-void LEDM_Manage(void)
-{
-    
+
+void LED_Manage(void) {
+    WDGM_AlivenessIndication();
+
+    Counter += 10; 
+
+    if (Counter >= 500) {                //if statement toggle the led state every 500 ms
+       led_State = !led_State
+        GPIO_Write(LED_PIN, led_State);
+        Counter = 0;
+    }
+
+    _delay_ms(10);                          //  delay function  for 10 ms
 }

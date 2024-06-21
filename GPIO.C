131539f50906e7c7d2f53e2e35a35d4b18d55a42
Omarmo2025 <166153836+Omarmo2025@users.noreply.github.com>
2024-06-22 01:40:08 +0300

    Update GPIO.C

--- GPIO.C
+++ GPIO.C
@@ -8,14 +8,20 @@
     TEAM NUMBER: 
 
 */
-#include "WDGM.h"
+
 #include "Std_Types.h"
+#include "GPIO.h"
+#define LED_PIN1 PA0   // the pin that we will use 
 
-void GPIO_Init(void)
-{
-
+void GPIO_Init(void) {
+    
+    DDRA |= (1 << LED_PIN1); // Set pin 0 port A as output
 }
-void GPIO_Write(unsigned char PinId, unsigned char PinData)
-{
 
+void GPIO_Write(unsigned char PinId, unsigned char PinData) {
+    if (PinData) {
+        PORTA |= (1 << PinId); // write high on the pin 
+    } else {
+        PORTA &= ~(1 << PinId); //write low on the pin
+    }
 }

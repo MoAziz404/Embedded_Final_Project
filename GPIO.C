95170521aacc50e0df2a771f8908a25c98af82bd
Omarmo2025 <166153836+Omarmo2025@users.noreply.github.com>
2024-06-22 15:58:03 +0300

    Update GPIO.C

--- GPIO.C
+++ GPIO.C
@@ -11,11 +11,14 @@
 
 #include "Std_Types.h"
 #include "GPIO.h"
-#define LED_PIN1 PA0   // the pin that we will use 
-
+#define LED_PIN1 PA1   // the pins that we will use 
+#define LED_PIN2 PA2
+#define LED_PIN3 PA3
+#define LED_PIN4 PA4
 void GPIO_Init(void) {
     
-    DDRA |= (1 << LED_PIN1); // Set pin 0 port A as output
+    DDRA |= (1 << LED_PIN1)|(1 << LED_PIN2)|(1 << LED_PIN3)|(1 << LED_PIN4); 
+// Set pins 1,2,3and 4 port A as output
 }
 
 void GPIO_Write(unsigned char PinId, unsigned char PinData) {
